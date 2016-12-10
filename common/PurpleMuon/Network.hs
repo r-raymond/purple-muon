2bf40932d42951c8d4dc42fdfbbd201603c5c5f9
Robin Raymond <robin@robinraymond.de>
2016-12-10 16:33:35 +0100

    starts network implementation
    So far it implements prepending messages with an uuid and checking the
    messages for these.

---
+++ common/PurpleMuon/Network.hs
@@ -0,0 +1,32 @@
+module PurpleMuon.Network
+    (
+    ) where
+
+import Protolude
+
+import qualified Data.ByteString as DBS
+
+-- | A binary message that is send over the network
+newtype RawMessage = RawMessage { unRawMessage :: ByteString }
+
+-- | A binary message without a uuid header
+newtype NakedMessage = NakedMessage { unNakedMessage :: ByteString }
+
+-- | A uuid is a header that the server and client prepends to
+-- every message. A message that does not have such a header is
+-- discarded immidieately
+newtype UUID = UUID { unUUID :: ByteString }
+
+-- | Prepend a message with an uuid.
+prepend :: UUID -> NakedMessage -> RawMessage
+prepend uuid msg = RawMessage ((unUUID uuid) <> (unNakedMessage msg))
+
+-- | strip an uuid off of a message.
+-- Returns Just (stripped message) if the message contains the correct uuid
+-- and Nothing otherwise
+strip :: UUID -> RawMessage -> Maybe NakedMessage
+strip uuid msg = if (u == h) then (Just $ NakedMessage t) else Nothing
+  where
+    u = (unUUID uuid)
+    l = DBS.length u
+    (h, t) = DBS.splitAt l (unRawMessage msg)
