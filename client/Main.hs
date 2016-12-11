41daa7a95babccf1cb91485e6e481b94d1054554
Robin Raymond <robin@robinraymond.de>
2016-12-11 17:55:38 +0100

    proof of concept udp connection

--- client/Main.hs
+++ client/Main.hs
@@ -2,5 +2,12 @@ module Main where
 
 import Protolude
 
+import Network.Socket.ByteString
+
+import PurpleMuon.Network.Util
+
 main :: IO ()
-main = return ()
+main = do
+    (Right cs) <- clientSocket "127.0.0.1" "7123"
+    _ <- send cs "Hello World"
+    return ()
--- server/Main.hs
+++ server/Main.hs
@@ -2,5 +2,12 @@ module Main where
 
 import Protolude
 
+import Network.Socket.ByteString
+
+import PurpleMuon.Network.Util
+
 main :: IO ()
-main = return ()
+main = do
+    (Right ss) <- serverSocket "7123"
+    (m, a) <- recvFrom ss 1024
+    putStrLn ("Received: " ++ (toS m) ++ " from " ++ (show a))
