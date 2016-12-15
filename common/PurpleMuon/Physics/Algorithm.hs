6e64f587970453058742d7f134df0a6bb6237d60
Robin Raymond <robin@robinraymond.de>
2016-12-15 18:10:36 +0100

    fix algorithms from the change of the types

--- common/PurpleMuon/Physics/Algorithm.hs
+++ common/PurpleMuon/Physics/Algorithm.hs
@@ -11,12 +11,14 @@ module PurpleMuon.Physics.Algorithm
     ( integrateObject
     , gravitationalForceVec
     , calculateGravitationalForces
+    , resetForces
     ) where
 
-import Protolude
+import           Protolude
 
-import qualified Linear.Metric as LME
-import qualified Linear.V2 as LV2
+import qualified Control.Lens             as CLE
+import qualified Linear.Metric            as LME
+import qualified Linear.V2                as LV2
 
 import qualified PurpleMuon.Physics.Types as PPT
 
@@ -29,101 +31,66 @@ integrateVel (PPT.DeltaTime dt) (PPT.Velocity v) (PPT.Position p) = PPT.Position
 newton2nd :: PPT.Force -> PPT.Mass -> PPT.Acceleration
 newton2nd (PPT.Force f) (PPT.Mass m) = PPT.Acceleration (fmap (/ m) f)
 
-integrateObject :: PPT.DeltaTime -> PPT.DynamicObject -> PPT.Force -> PPT.DynamicObject
-integrateObject dt (PPT.DynamicObject (PPT.StaticObject (p, m), v)) f =
-    PPT.DynamicObject ((PPT.StaticObject (pnew, m)), vnew)
+-- | Integrate a physical object.
+-- Uses the force saved in the object and applies is to the physical object
+-- So far ignore the fact if the object is static. Should this be changed?
+integrateObject :: PPT.DeltaTime -> PPT.PhysicalObject -> PPT.PhysicalObject
+integrateObject dt po =
+    CLE.set PPT.pos pnew $
+    CLE.set PPT.vel vnew po
       where
-        a = newton2nd f m
-        vnew = integrateAccel dt a v
-        pnew = integrateVel dt vnew p
+        a = newton2nd (CLE.view PPT.force po) (CLE.view PPT.mass po)
+        vnew = integrateAccel dt a (CLE.view PPT.vel po)
+        pnew = integrateVel dt vnew (CLE.view PPT.pos po)
 
 -- |The gravitational force between to objects
 -- This only calculates the magnitude of the force
-gravitationalForce :: PPT.GravitationalConstant -> PPT.StaticObject -> PPT.StaticObject -> Float
-gravitationalForce (PPT.GravitationalConstant g)
-                   (PPT.StaticObject (PPT.Position p1, PPT.Mass m1))
-                   (PPT.StaticObject (PPT.Position p2, PPT.Mass m2)) =
+gravitationalForce :: PPT.GravitationalConstant -> PPT.PhysicalObject -> PPT.PhysicalObject -> Float
+gravitationalForce (PPT.GravitationalConstant g) po1 po2 =
     g * m1 * m2 / (LME.qd p1 p2)
+      where
+        m1 = PPT.unMass $ CLE.view PPT.mass po1
+        m2 = PPT.unMass $ CLE.view PPT.mass po2
+        p1 = PPT.unPosition $ CLE.view PPT.pos po1
+        p2 = PPT.unPosition $ CLE.view PPT.pos po2
 
 -- |The graviational forces between to objects
 -- Returns the gravitaitonal force on the first object. The force on the
 -- second one is of course just the negative of the returned force.
-gravitationalForceVec :: PPT.GravitationalConstant -> PPT.StaticObject -> PPT.StaticObject -> PPT.Force
-gravitationalForceVec g
-                      o1@(PPT.StaticObject (PPT.Position p1, _))
-                      o2@(PPT.StaticObject (PPT.Position p2, _)) = PPT.Force $
-    fmap (* (gravitationalForce g o1 o2)) (LME.signorm (p2 - p1))
-
-dynToSt :: PPT.DynamicObject -> PPT.StaticObject
-dynToSt (PPT.DynamicObject (s, _)) = s
-
-grToSt :: PPT.GravitatingObject -> PPT.StaticObject
-grToSt (PPT.GravitatingObject x) = dynToSt x
+gravitationalForceVec :: PPT.GravitationalConstant -> PPT.PhysicalObject -> PPT.PhysicalObject -> PPT.Force
+gravitationalForceVec g po1 po2 = PPT.Force $
+    fmap (* (gravitationalForce g po1 po2)) (LME.signorm (p2 - p1))
+      where
+        p1 = PPT.unPosition $ CLE.view PPT.pos po1
+        p2 = PPT.unPosition $ CLE.view PPT.pos po2
 
 addForce :: PPT.Force -> PPT.Force -> PPT.Force
 addForce (PPT.Force f1) (PPT.Force f2) = PPT.Force (f1 + f2)
 
--- | Save calulcation of gravitational forces
--- Does check if objects happen to be the same (in which case the force will be
--- zero) and also sets a minimum distance so that the algorithm becomes
--- numerically more stable
-saveGravitationalForceVec :: Float -> PPT.GravitationalConstant -> PPT.StaticObject -> PPT.StaticObject -> PPT.Force
-saveGravitationalForceVec eps g o1 o2 = if o1 /= o2
-                                            then gravitationalForceVec g o1 o2
-                                            else PPT.Force $ LV2.V2 0 0
-                                            -- TODO Implement eps
+-- | Reset all forces to 0
+resetForces :: [PPT.PhysicalObject] -> [PPT.PhysicalObject]
+resetForces = fmap (CLE.set PPT.force (PPT.Force $ LV2.V2 0 0))
 
 -- | Calculate all forces for a system.
--- For performance reasons we differentiate between objects that only receive
--- gravitating forces and objects that both receive and induce gravitating
--- forces
+-- Does not reset the forces, needs to be done manually before
 calculateGravitationalForces :: PPT.GravitationalConstant -- ^ Gravitational Constant
-                             -> [PPT.StaticObject] -- ^ Objects that only gravitate
-                             -> [PPT.GravitatingObject] -- ^ Objects that both gravitate and move
-                             -> [PPT.DynamicObject] -- ^ Objects that only move
-                             -> ( [(PPT.GravitatingObject, PPT.Force)]
-                                , [(PPT.DynamicObject, PPT.Force)] )
-calculateGravitationalForces g so go ro = dynamicForces g grWithF2 roWithF2
-    where
-      goWithF = fmap (\x -> (x, PPT.Force $ LV2.V2 0 0)) go
-      roWithF = fmap (\x -> (x, PPT.Force $ LV2.V2 0 0)) ro
-      (grWithF2, roWithF2) = staticForces g so goWithF roWithF
+                             -> [PPT.PhysicalObject]      -- ^ The physical objects to integrate
+                             -> [PPT.PhysicalObject]
+calculateGravitationalForces g pos = undefined
+      where
+        staticObjs      = filter (CLE.view PPT.static) pos
+        nonStaticObjs   = filter (not . (CLE.view PPT.static)) pos
+        gravitatingObjs = filter (CLE.view PPT.gravitating) nonStaticObjs
+        dynamicObjs     = filter (not . (CLE.view PPT.gravitating)) nonStaticObjs
 
 
--- | Helper function for only the static forces
-staticForces :: PPT.GravitationalConstant
-            -> [PPT.StaticObject]
-            -> [(PPT.GravitatingObject, PPT.Force)]
-            -> [(PPT.DynamicObject, PPT.Force)]
-            -> ( [(PPT.GravitatingObject, PPT.Force)]
-               , [(PPT.DynamicObject, PPT.Force)] )
-staticForces g so go ro = (graObj, dynObj)
-  where
-    helper :: PPT.StaticObject -> (PPT.DynamicObject, PPT.Force) -> (PPT.DynamicObject, PPT.Force)
-    helper = foldingForceHelper g identity dynToSt
-    helper2 :: PPT.StaticObject -> (PPT.GravitatingObject, PPT.Force) -> (PPT.GravitatingObject, PPT.Force)
-    helper2 = foldingForceHelper g identity grToSt
-    dynObj = fmap (\x -> foldr' helper x so) ro
-    graObj = fmap (\x -> foldr' helper2 x so) go
-
--- | Helper function for only the dynamic forces
-dynamicForces :: PPT.GravitationalConstant
-              -> [(PPT.GravitatingObject, PPT.Force)]
-              -> [(PPT.DynamicObject, PPT.Force)]
-              -> ( [(PPT.GravitatingObject, PPT.Force)]
-                 , [(PPT.DynamicObject, PPT.Force)] )
-dynamicForces g go ro = (graObj, dynObj)
-  where
-    helper :: PPT.GravitatingObject -> (PPT.DynamicObject, PPT.Force) -> (PPT.DynamicObject, PPT.Force)
-    helper = foldingForceHelper g grToSt dynToSt
-    helper2 :: PPT.GravitatingObject -> (PPT.GravitatingObject, PPT.Force) -> (PPT.GravitatingObject, PPT.Force)
-    helper2 = foldingForceHelper g grToSt grToSt
-    dynObj = fmap (\x -> foldr' helper x (map fst go)) ro
-    graObj = fmap (\x -> foldr' helper2 x (map fst go)) go
-
-
-foldingForceHelper :: PPT.GravitationalConstant -> (a -> PPT.StaticObject) -> (b -> PPT.StaticObject) -> a -> (b, PPT.Force) -> (b, PPT.Force)
-foldingForceHelper g xts yts x (y, f) = (y, addForce f (gravitationalForceVec g (yts y) (xts x)))
-
-
-
+-- |Apply the force of the first object to the second
+-- Ignores all static / gravitating modifier but checks that the objects
+-- are not the same
+applyForce :: PPT.GravitationalConstant -> PPT.PhysicalObject -> PPT.PhysicalObject -> PPT.PhysicalObject
+applyForce g o1 o2 =
+    if o1 /= o2
+        then CLE.over PPT.force (addForce f) o2
+        else o2
+      where
+        f = gravitationalForceVec g o1 o2
