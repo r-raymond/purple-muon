7deb111882a06a98622cf9b38c38846021a3d5d6
Robin Raymond <robin@robinraymond.de>
2017-01-17 17:18:28 +0100

    add licensing info to all files

--- client/Client/Video/Types.hs
+++ client/Client/Video/Types.hs
@@ -1,3 +1,20 @@
+--  Copyright 2016, 2017 Robin Raymond
+--
+--  This file is part of Purple Muon
+--
+--  Purple Muon is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  Purple Muon is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with Purple Muon.  If not, see <http://www.gnu.org/licenses/>.
+
 module Client.Video.Types
     ( TextureAtlas(..)
     , Texture(..)
