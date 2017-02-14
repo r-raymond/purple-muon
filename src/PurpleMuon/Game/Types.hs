368300a3b1de5b1a9fc66bb722e33fa068586508
Robin Raymond <robin@robinraymond.de>
2017-02-14 17:20:50 +0100

    fix haddock comment

--- src/PurpleMuon/Game/Types.hs
+++ src/PurpleMuon/Game/Types.hs
@@ -1,7 +1,19 @@
-module PurpleMuon.Game.Types
-    ( GameObject(..)
-    , GameObjUUID(..)
-    ) where
+--  Copyright 2016, 2017 Robin Raymond
+--
+--  This file is part of Purple Muon
+--
+--  Purple Muon is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  Purple Muon is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with Purple Muon.  If not, see <http://www.gnu.org/licenses/>.
 
 {-|
 Module      : PurpleMuon.Network.Types
@@ -11,6 +23,11 @@ License     : GPL-3
 Maintainer  : robin@robinraymond.de
 Portability : POSIX
 -}
+
+module PurpleMuon.Game.Types
+    ( GameObject(..)
+    , GameObjUUID(..)
+    ) where
 
 import Protolude
 
